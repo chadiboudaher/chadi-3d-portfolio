54aca125a58019314b94512817feccac4d260b59
chadiboudaher <chadiboudaher7@gmail.com>
2026-09-19 21:38:04 +0300

    feat: add experience three component

---
+++ src/three/Experience.jsx
@@ -0,0 +1,15 @@
+import CameraRig from "./CameraRig";
+import Lighting from "./Lighting";
+import PortfolioModel from "./PortfolioModel";
+
+export default function Experience({ onLoaded }) {
+  return (
+    <>
+      <color attach="background" args={["#ffe29a"]} />
+      <fog attach="fog" args={["#ffe29a", 24, 55]} />
+      <Lighting />
+      <PortfolioModel onLoaded={onLoaded} />
+      <CameraRig />
+    </>
+  );
+}
